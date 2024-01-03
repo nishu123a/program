7c918a869de2aaa52d08267e84206ef9a8c156c5
nishu123a <16012002nishukumarisinha@gmail.com>
2024-01-03 18:13:49 +0530

    Your commit message here

---
+++ src/user/UserDashBoard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Base from '../core/Base'
+const UserDashboard=()=>{ 
+    return(
+<Base title="UserDashboard Page">
+<h1>This is UserDashboard </h1>
+</Base>   
+    )
+}
+export default UserDashboard;
